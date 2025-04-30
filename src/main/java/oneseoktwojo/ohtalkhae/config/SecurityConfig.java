a3c81087aaecfc62a7c721e9b0d5e0723478825f
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-04-30 13:37:33 +0900

    Implement security configuration with JWT authentication and password encoding

--- src/main/java/oneseoktwojo/ohtalkhae/config/SecurityConfig.java
+++ src/main/java/oneseoktwojo/ohtalkhae/config/SecurityConfig.java
@@ -1,8 +1,58 @@
 package oneseoktwojo.ohtalkhae.config;
 
+import oneseoktwojo.ohtalkhae.domain.auth.jwt.LoginFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    private final AuthenticationConfiguration authenticationConfiguration;
+
+    @Autowired
+    public SecurityConfig(AuthenticationConfiguration authenticationConfiguration) {
+        this.authenticationConfiguration = authenticationConfiguration;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(formLogin -> formLogin.loginProcessingUrl("/auth/login").disable())
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorizeRequests -> {
+                    authorizeRequests
+                            .requestMatchers("/auth/register").permitAll()
+                            .requestMatchers("/auth/login").permitAll()
+                            .requestMatchers("/auth/refresh").permitAll()
+                            .requestMatchers("/auth/logout").permitAll()
+                            .requestMatchers("/auth/verify").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .addFilterAt(new LoginFilter(authenticationManager(authenticationConfiguration)), UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+    }
 }
