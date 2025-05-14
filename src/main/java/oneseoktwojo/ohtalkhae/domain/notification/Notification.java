693296b7637df2f11db734d4dc1607cd9aaf9511
Woochang Lee <changi112242@gmail.com>
2025-05-14 14:22:10 +0900

    Implement Notification entity and repository

---
+++ src/main/java/oneseoktwojo/ohtalkhae/domain/notification/Notification.java
@@ -0,0 +1,37 @@
+package oneseoktwojo.ohtalkhae.domain.notification;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import oneseoktwojo.ohtalkhae.domain.notification.enums.NotificationType;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Notification {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long notificationId;
+
+    private Long userId;
+    @Enumerated(EnumType.STRING)
+    private NotificationType type;
+
+    private String title;
+    @Column(length = 200)
+    private String message;
+    @Column(length = 1000)
+    private String clickTarget;
+
+    private boolean isRead;
+    private boolean isDeleted;
+    private LocalDateTime createdAt;
+
+}
