26f374b2b3ca914da3eb6023b6608f00f6b04908
Huey S. Jeong <wjdgnltkd123@gmail.com>
2025-05-15 17:29:14 +0900

    feat: Replace ApiResponse with ResponseEntity in controllers and exception handlers

--- src/main/java/oneseoktwojo/ohtalkhae/global/exception/ExController.java
+++ src/main/java/oneseoktwojo/ohtalkhae/global/exception/ExController.java
@@ -3,6 +3,7 @@ package oneseoktwojo.ohtalkhae.global.exception;
 import lombok.extern.slf4j.Slf4j;
 import oneseoktwojo.ohtalkhae.global.dto.ApiResponse;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.validation.FieldError;
@@ -18,7 +19,7 @@ import java.util.Map;
 @RestControllerAdvice
 public class ExController {
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ApiResponse<?> handleValidationExceptions(
+    public ResponseEntity<ApiResponse<?>> handleValidationExceptions(
             MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
 
@@ -28,29 +29,29 @@ public class ExController {
             errors.put(fieldName, errorMessage);
         });
 
-        return ApiResponse.error(400, "Invalid Values", errors);
+        return ApiResponse.error(HttpStatus.BAD_REQUEST, "Invalid Values", errors);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
-    public ApiResponse<?> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
+    public ResponseEntity<ApiResponse<?>> handleHttpMessageNotReadable(HttpMessageNotReadableException ex) {
         log.debug(ex.getMessage());
-        return ApiResponse.error(400, "Invalid Request Body");
+        return ApiResponse.error(HttpStatus.BAD_REQUEST, "Invalid Request Body");
     }
     
     @ExceptionHandler(IllegalArgumentException.class)
-    public ApiResponse<?> handleIllegalArgumentException(IllegalArgumentException ex) {
-        return ApiResponse.error(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+    public ResponseEntity<ApiResponse<?>> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ApiResponse.error(HttpStatus.BAD_REQUEST, "Invalid Request", ex.getMessage());
     }
 
     @ExceptionHandler(IOException.class)
-    public ApiResponse<?> handleIOException(IOException ex) {
-        return ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR.value(), "프로필 사진 업로드에 실패했습니다 : " + ex.getMessage());
+    public ResponseEntity<ApiResponse<?>> handleIOException(IOException ex) {
+        return ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, "프로필 사진 업로드에 실패했습니다 : " + ex.getMessage());
     }
 
     @ExceptionHandler(AuthenticationException.class)
-    public ApiResponse<?> handleGeneralException(Exception ex) {
+    public ResponseEntity<ApiResponse<?>> handleGeneralException(Exception ex) {
         log.debug(ex.getMessage());
-        return ApiResponse.error(401, "Unauthorized");
+        return ApiResponse.error(HttpStatus.UNAUTHORIZED, "Unauthorized");
     }
 //    @ExceptionHandler
 }
